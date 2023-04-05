839067716aa973f0b588dbaae01268b7d567aed2
odanzhou <odanzhou@gmail.com>
2023-04-05 18:09:05 +0800

    feat: 文件监听，成功了1%，需要的是重启webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,37 @@
-const Webpack = require('webpack')
+const webpack = require('webpack')
 const path = require('path')
 const fs = require('fs')
 const JSONC = require('jsonc-parser')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { ModuleFederationPlugin } = Webpack.container
+
+const { container: { ModuleFederationPlugin } } = webpack
+
+
+class TestPlugin {
+  constructor (files) {
+    this.count = 0
+    this.watchFiles = Array.isArray(files) ? files : []
+  }
+  apply (compiler) {
+    console.log('>>>>>>>>>>>applyapplyapplyapply>>>>>>>>>>>>')
+    compiler.hooks.compilation.tap('MyPlugin', (compilation) => {
+      this.count += 1
+      console.log('argsxxx>>>>>>>>>>>>', this.count, this.watchFiles, compilation.fileDependencies)
+      if(compilation.fileDependencies && this.watchFiles.length) {
+        compilation.fileDependencies.addAll(this.watchFiles)
+        console.log('yyyyxxx', compilation.fileDependencies.size)
+      }
+    })
+  }
+}
 
 // 共用path 的可以搞个 npm 包
 function readJson(jsonPath) {
   const jsonText = fs.readFileSync(jsonPath, "utf8");
   return JSONC.parse(jsonText);
 }
+
+console.log('__dirname', __dirname)
 
 const TSConfig = readJson('./tsconfig.json', 'utf-8')
 const TSConfPaths = TSConfig.compilerOptions.paths
@@ -23,7 +45,7 @@ const resolvePaths = Object.entries(TSConfPaths).reduce((res, [key, list]) => {
   return res
 }, {})
 /**
- * @type { Webpack.WebpackOptionsNormalized }
+ * @type { webpack.WebpackOptionsNormalized }
  */
 module.exports = {
   mode: 'development',
@@ -56,7 +78,7 @@ module.exports = {
     historyApiFallback: true,
     // https://webpack.docschina.org/configuration/dev-server/#devserverallowedhosts
     // 当设置为 'auto' 时，此配置项总是允许 localhost、 host 和 client.webSocketURL.hostname：
-    allowedHosts: 'auto', // 'all'
+    allowedHosts: 'all', // 'auto'|'all'
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -65,14 +87,15 @@ module.exports = {
       filename: 'remoteEntry.js', // 入口文件名称，用于对外提供模块时候的入口文件名
       exposes: { // 需要导出的模块，用于提供给外部其他项目进行使用
         './search': './src/pages/Search', // '@/src/pages/Search' // 不能用@的别用方式
-      },
+      }, 
       remotes: {
         libA: 'webpackBHost@http://localhost:9100/remoteEntry.js'
       },
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(cwdPath, 'public/index.html')
-    })
+    }),
+    new TestPlugin([__filename]),
   ],
   watchOptions: {
     ignored: /node_modules/,
